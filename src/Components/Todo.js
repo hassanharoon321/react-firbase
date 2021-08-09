371104e03d74514fba14790daa8da8596a19f123
hassan haroon <hassanharoon321@gmail.com>
2021-08-10 01:08:15 +0500

    aded

---
+++ src/Components/Todo.js
@@ -0,0 +1,13 @@
+import React,{useState} from 'react'
+import TodoForm from './TodoForm'
+
+function Todo() {
+    const[edit,setEdit]
+    return (
+        <div>
+            <TodoForm/>
+        </div>
+    )
+}
+
+export default Todo
